eb6339b481c17f168395199ab5ce4de790436ab6
nika <ivanidze.nika01@gmail.com>
2022-02-15 23:44:00 +0400

    about and academy pages

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -7,5 +7,269 @@
 @endsection
 
 @section('footer')
+<div class="home-page-header">
+    <div class="home-page-header-middle">
+        <div class="home-page-header-inner">
+            <div class="home-page-header-title">
+                <p>ABOUT COMPANY</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="home-part-1">
+  <div class="home-part-1-middle">
+    <div class="home-part-1-inner">
+        <div class="home-about-us-info">
+          
+
+          <div class="home-about-us-title">
+            <p>BENE EXCLUSIVE</p>
+          </div>
+
+          <div class="home-about-us-text">
+            <p>"Bene-Exclusive" is a company specializing in logistics working under the umbrella of "Bene-Group".
+                Initially, the services of our company were limited to premium class vehicles, however today "Bene-Ecxlusive" 
+                represents a multi-profile company. Car renting as well as orginizing of Business Projects and Events have been
+                added to our list of services. Exclusive" is a company specializing in logistics working under the
+                umbrella of "Bene-Group". Initially, the services of our company were limited to premium class vehicles,
+                however today "Bene-Ecxlusive" represents a multi-profile company. Car renting as well as orginizing of
+                Business Projects and Events have been added to our list of services.
+            </p>
+
+          </div>
+
+          
+          
+        </div>
+
+        <div class="home-about-us-video">
+          <embed src="https://www.youtube.com/embed/W7vWuPWh4-Y?autoplay=1&mute=1" frameborder="0"></embed>
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+<div class="about-page-part-2">
+    <div class="about-page-part-2-middle">
+        <div class="about-page-part-2-inner">
+            <div class="about-page-events">
+
+                <div class="about-page-events-picture">
+                    <img src="{{asset('front/images/about-2.png') }}" alt="">
+                </div>
+
+
+                <div class="about-page-events-text">
+                    <div class="about-page-company-name">
+                        <p>
+                            BENE EXCLUSIVE
+                        </p>
+                    </div>
+
+                    <div class="about-page-events-title">
+                        <p>
+                            EVENTS
+                        </p>
+                    </div>
+
+                    <div class="about-page-events-info">
+                        <p>
+                         "Bene-Exclusive" is a company specializing in logistics working under the umbrella of "Bene-Group".
+                          Initially, the services of our company were limited to premium class vehicles, however today "Bene-Ecxlusive"
+                          represents a multi-profile company. Car renting as well as orginizing of Business Projects and Events have 
+                          been added to our list of services. "Bene-Exclusive" is a company specializing in logistics working under the
+                          umbrella of "Bene-Group". Initially, the services of our company were limited to premium class vehicles, however
+                          today "Bene-Ecxlusive" represents a multi-profile company. Car renting as well as orginizing of Business Projects 
+                          and Events have been added to our list of services.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<div class="about-page-part-3">
+    <div class="about-page-part-3-middle">
+        <div class="about-page-part-3-inner">
+
+            <div class="about-vehicle-renewals">
+
+
+                <div class="about-vehicle-renewal-info">
+                    <div class="about-vehicle-renewal-title">
+                        <p>
+                            VEHICLE RENEWALS
+                        </p>
+                    </div>
+
+                    <div class="about-vehicle-renewal-text">
+                        <p>
+                        "Bene-Exclusive" is a company specializing in logistics working under the umbrella of "Bene-Group".
+                          Initially, the services of our company were limited to premium class vehicles, however today "Bene-Ecxlusive"
+                          represents a multi-profile company. Car renting as well as orginizing of Business Projects and Events have 
+                          been added to our list of services. "Bene-Exclusive" is a company specializing in logistics working under the
+                          umbrella of "Bene-Group". Initially, the services of our company were limited to premium class vehicles, however
+                          today "Bene-Ecxlusive" represents a multi-profile company. Car renting as well as orginizing of Business Projects 
+                          and Events have been added to our list of services.
+                        </p>
+                    </div>
+
+                </div>
+
+
+                <div class="about-vehicle-renewal-image">
+                    <img src="{{asset('front/images/about-1.png') }}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="index-part-2">
+  <div class="index-part-2-middle">
+    <div class="index-part-2-inner">
+      <div class="index-news-header">
+        <div class="index-news-header-title">
+          <p>NEWSROOM</p>
+        </div>
+
+        <div class="see-all-button">
+          <button>
+            <p>See all</p>
+            <i class="fas fa-arrow-right"></i>
+          </button>
+        </div>
+
+
+      </div>
+
+      
+      <div class="index-news-body">
+          <div class="index-news-article">
+            <div class="index-news-image">
+              <img src="{{asset('front/images/news-1.png') }}" alt="">
+            </div>
+
+            <div class="index-news-text">
+              <div class="news-date">
+              <p>February 4, 2022</p>
+              </div>
+
+              <h6>BENE EXCLUSIVE ფარიკაობის მსოფლიო ჩემპიონატის...</h6>
+
+              <p>2022 წლის პირველი მასშტაბური საერთაშორისო ტურნირი ფარიკაობაში საქართველოში ტარდება. მსოფლიო თასის გათამაშება თბილისის ოლიმპიურ სასახლეში 14-17 იანვარს გაიმართება...</p>
+
+
+              <div class="index-read-more">
+                <h5>Read more</h5>
+                <i class="fas fa-arrow-right"></i>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="index-news-article">
+            <div class="index-news-image">
+              <img src="{{asset('front/images/news-2.png') }}" alt="">
+            </div>
+
+            <div class="index-news-text">
+              <div class="news-date">
+              <p>February 4, 2022</p>
+              </div>
+
+              <h6>BENE EXCLUSIVE ფარიკაობის მსოფლიო ჩემპიონატის...</h6>
+
+              <p>2022 წლის პირველი მასშტაბური საერთაშორისო ტურნირი ფარიკაობაში საქართველოში ტარდება. მსოფლიო თასის გათამაშება თბილისის ოლიმპიურ სასახლეში 14-17 იანვარს გაიმართება...</p>
+
+
+              <div class="index-read-more">
+                <h5>Read more</h5>
+                <i class="fas fa-arrow-right"></i>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="index-news-article">
+            <div class="index-news-image">
+              <img src="{{asset('front/images/news-3.png') }}" alt="">
+            </div>
+
+            <div class="index-news-text">
+              <div class="news-date">
+              <p>February 4, 2022</p>
+              </div>
+
+              <h6>BENE EXCLUSIVE ფარიკაობის მსოფლიო ჩემპიონატის...</h6>
+
+              <p>2022 წლის პირველი მასშტაბური საერთაშორისო ტურნირი ფარიკაობაში საქართველოში ტარდება. მსოფლიო თასის გათამაშება თბილისის ოლიმპიურ სასახლეში 14-17 იანვარს გაიმართება...</p>
+
+
+              <div class="index-read-more">
+                <h5>Read more</h5>
+                <i class="fas fa-arrow-right"></i>
+              </div>
+            </div>
+          </div>
+
+
+
+          <div class="index-news-article">
+            <div class="index-news-image">
+              <img src="{{asset('front/images/news-4.png') }}" alt="">
+            </div>
+
+            <div class="index-news-text">
+              <div class="news-date">
+              <p>February 4, 2022</p>
+              </div>
+
+              <h6>BENE EXCLUSIVE ფარიკაობის მსოფლიო ჩემპიონატის...</h6>
+
+              <p>2022 წლის პირველი მასშტაბური საერთაშორისო ტურნირი ფარიკაობაში საქართველოში ტარდება. მსოფლიო თასის გათამაშება თბილისის ოლიმპიურ სასახლეში 14-17 იანვარს გაიმართება...</p>
+
+
+              <div class="index-read-more">
+                <h5>Read more</h5>
+                <i class="fas fa-arrow-right"></i>
+              </div>
+            </div>
+          </div>
+
+
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="index-part-8">
+  <div class="part-8-middle">
+    <div class="part-8-inner">
+      <div class="index-partners-header"><p>PARTNERS</p></div>
+
+      <div class="index-partners-logo">
+        <div><img src="{{asset('front/images/partners-logo-2.png') }}" alt=""></div>
+        <div><img src="{{asset('front/images/partners-logo-1.png') }}" alt=""></div>
+        <div><img src="{{asset('front/images/partners-logo-2.png') }}" alt=""></div>
+        <div><img src="{{asset('front/images/partners-logo-1.png') }}" alt=""></div>
+        <div><img src="{{asset('front/images/partners-logo-2.png') }}" alt=""></div>
+        <div><img src="{{asset('front/images/partners-logo-1.png') }}" alt=""></div>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
