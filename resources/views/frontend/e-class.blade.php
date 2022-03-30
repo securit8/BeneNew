384f96d10fa42e9b4a007978b8db9f7992c96c16
nika <ivanidze.nika01@gmail.com>
2022-03-30 12:44:48 +0400

    updated

--- resources/views/frontend/e-class.blade.php
+++ resources/views/frontend/e-class.blade.php
@@ -20,23 +20,33 @@
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="3" aria-label="Slide 4"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="4" aria-label="Slide 5"></button>
     </div>
   </div>
 
 
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="{{asset('front/images/spec-carousel-1.png')}}" class="" alt="...">
+      <img src="{{asset('front/images/e-class/1.jpg')}}" class="" alt="...">
     </div>
 
 
     <div class="carousel-item">
-      <img src="{{asset('front/images/spec-carousel-1.png')}}" class="" alt="...">
+      <img src="{{asset('front/images/e-class/2.jpg')}}" class="" alt="...">
     </div>
 
 
     <div class="carousel-item">
-      <img src="{{asset('front/images/spec-carousel-1.png')}}" class="" alt="...">
+      <img src="{{asset('front/images/e-class/3.jpg')}}" class="" alt="...">
+    </div>
+
+    <div class="carousel-item">
+      <img src="{{asset('front/images/e-class/4.jpg')}}" class="" alt="...">
+    </div>
+
+    <div class="carousel-item">
+      <img src="{{asset('front/images/e-class/5.jpg')}}" class="" alt="...">
     </div>
 
 
--- resources/views/frontend/land200.blade.php
+++ resources/views/frontend/land200.blade.php
@@ -20,6 +20,7 @@
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="3" aria-label="Slide 4"></button>
     </div>
   </div>
 
@@ -31,12 +32,16 @@
 
 
     <div class="carousel-item">
-      <img src="{{asset('front/images/200/1.jpg')}}" class="" alt="...">
+      <img src="{{asset('front/images/200/2.jpg')}}" class="" alt="...">
     </div>
 
 
     <div class="carousel-item">
-      <img src="{{asset('front/images/200/1.jpg')}}" class="" alt="...">
+      <img src="{{asset('front/images/200/3.jpg')}}" class="" alt="...">
+    </div>
+
+    <div class="carousel-item">
+      <img src="{{asset('front/images/200/4.jpg')}}" class="" alt="...">
     </div>
 
 
