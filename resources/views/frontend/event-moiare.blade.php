98b0c3abae639d40ab5e8ebcdd13e234e5f768ea
nika <ivanidze.nika01@gmail.com>
2022-03-31 15:19:32 +0400

    thursday

--- resources/views/frontend/event-moiare.blade.php
+++ resources/views/frontend/event-moiare.blade.php
@@ -93,102 +93,31 @@
 
 
 
-                            <div class="index-part-7">
-                <div class="part-7-middle">
-                    <div class="part-7-inner">
-                        <div class="index-event-header"><p>EVENTS</p></div>
+          
+<div class="index-part-7">
+  <div class="part-7-middle">
+    <div class="part-7-inner">
+        <div class="index-event-header"><p>@lang('front.index-events')</p></div>
 
-                    <div id="carouselExampleIndicators4" class="carousel slide" data-bs-ride="carousel">
-                        <div class="carousel-indicators">
-                        <div id="index-event-carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators4" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators4" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators4" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                        </div>
-                        </div>
-                        <div class="carousel-inner">
-                        <div class="index-event-carousel-inner">
-
-                            <div class="carousel-item active">
-                            <div class="index-event-carousel-item">
-                                
-                            <div class="event-1">
-                                <div class="event-1-backrgound">
-                                    <img src="{{asset('front/images/event-1-background.png') }}" class="" alt="...">
-                                </div>
-
-                                <div class="event-1-logo">
-                                    <img src="{{asset('front/images/event-1-logo.png') }}" class="" alt="...">
-                                </div>
-
-                                </div>
-
-                            <img src="{{asset('front/images/event-2.png') }}" class="d-block w-100" alt="...">   
-
-                            <img src="{{asset('front/images/event-3.png') }}" class="d-block w-100" alt="...">
-
-                            </div>
-                            </div>
-
-                            <div class="carousel-item">
-                            <div class="index-event-carousel-item">
-                                <div class="event-1">
-                                <div class="event-1-backrgound">
-                                    <img src="{{asset('front/images/event-1-background.png') }}" class="" alt="...">
-                                </div>
-
-                                <div class="event-1-logo">
-                                    <img src="{{asset('front/images/event-1-logo.png') }}" class="" alt="...">
-                                </div>
-
-                                </div>
-                            <img src="{{asset('front/images/event-2.png') }}" class="d-block w-100" alt="...">   
-
-                            <img src="{{asset('front/images/event-3.png') }}" class="d-block w-100" alt="...">
-                            </div>
-                            </div>
-
-                            <div class="carousel-item"> 
-                                <div class="index-event-carousel-item">
-                                <div class="event-1">
-                                <div class="event-1-backrgound">
-                                    <img src="{{asset('front/images/event-1-background.png') }}" class="" alt="...">
-                                </div>
-
-                                <div class="event-1-logo">
-                                    <img src="{{asset('front/images/event-1-logo.png') }}" class="" alt="...">
-                                </div>
-
-                                </div>
-                            <img src="{{asset('front/images/event-2.png') }}" class="d-block w-100" alt="...">   
-
-                            <img src="{{asset('front/images/event-3.png') }}" class="d-block w-100" alt="...">
-                            </div>
-                            </div>
-
-                        </div>
-                        </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators4" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon d-none" aria-hidden="true"></span>
-                        <span  class="carousel-arrow-icon"><img src="{{asset('front/images/carousel-right.png') }}" alt=""></span>
-                        <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators4" data-bs-slide="next">
-                        <span class="carousel-control-next-icon d-none" aria-hidden="true"></span>
-                        <span class="carousel-arrow-icon"><img src="{{asset('front/images/carousel-left.png') }}" alt=""></span>
-                        <span class="visually-hidden">Next</span>
-                        </button>
-                    </div>
-                                
-                        </div>
-                        </div>
-                    </div>
+      
+      <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><img src="{{asset('front/images/proeqti2.jpg')}}" alt=""></div>
+        <div class="swiper-slide"><img src="{{asset('front/images/proeqti3.jpg')}}" alt=""></div>
+        <div class="swiper-slide"><img src="{{asset('front/images/proeqti4.jpg')}}" alt=""></div>
+        <div class="swiper-slide"><img src="{{asset('front/images/proeqti5.jpg')}}" alt=""></div>
+        <div class="swiper-slide"><img src="{{asset('front/images/proeqti1.jpg')}}" alt=""></div>
+       
+      </div>
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+      <div class="swiper-pagination"></div>
+      </div>
 
 
-
-                    </div>
-                </div>
-                </div>
+    </div>
+  </div>
+</div>
 
 
 
