ff6039ea22a328395f64673d460ee53f23a28174
nika <ivanidze.nika01@gmail.com>
2022-04-06 11:58:44 +0400

    wednsday mediaquerry

--- resources/views/frontend/v-class.blade.php
+++ resources/views/frontend/v-class.blade.php
@@ -20,6 +20,7 @@
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators-specs" data-bs-slide-to="3" aria-label="Slide 4"></button>
     </div>
   </div>
 
@@ -31,9 +32,13 @@
 
 
     <div class="carousel-item">
-      <img src="{{asset('front/images/cropped3.JPG')}}" class="" alt="...">
+      <img src="{{asset('front/images/viano/2.jpg')}}" class="" alt="...">
     </div>
 
+
+    <div class="carousel-item">
+      <img src="{{asset('front/images/viano/4.jpg')}}" class="" alt="...">
+    </div>
 
     <div class="carousel-item">
       <img src="{{asset('front/images/viano/3.jpg')}}" class="" alt="...">
