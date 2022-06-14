73092d00eee4fa5cba1024a7bfafa0baa680b361
nika <ivanidze.nika01@gmail.com>
2022-06-15 00:47:30 +0400

    blacksea redesign

--- resources/views/frontend/blacksea.blade.php
+++ resources/views/frontend/blacksea.blade.php
@@ -103,34 +103,19 @@
 <p style="padding-bottom:20px;padding-left:30px;">ავტობუსი გადის წერეთლიდან, გამოფენის ტერიტორიის პარკინგიდან 10:00 საათზე</p>
   <form class="form-horizontal col-12 row" method="POST" action="{{ route('frontend.payzePost') }}">
     {{ csrf_field() }}
-    <div class = "col-12 row d-flex align-self-center" style = "border:1px solid black;padding:0;border-radius:20px;margin-bottom:20px;">
+    <div class = "col-12 row d-flex align-self-center " style = "border:1px solid black;padding:0;border-radius:20px;margin-bottom:20px;">
       <div class = "col-12 col-xl-5 d-flex justify-content-center text-center text-lg-start justify-content-xl-start  ticket_padding_top">
         <p  style="color:green;font-size:1rem;padding-top:5px;">თბილისი - Black Sea Arena (ერთი გზა)</p>
         <input type="hidden" name="transfer" value="tbilisi1" />
     </div>
 
-<<<<<<< HEAD
-    <div class = "col-12 col-sm-6 col-md-4 col-xl-3 d-flex justify-content-center justify-content-xl-end align-items-center">
+    <div class = "col-12 col-sm-6 col-md-4 col-xl-3 d-flex justify-content-center justify-content-xl-end align-items-center ticket_padding_top">
       <p style="color:black;font-size:1.5rem;padding-bottom: 1px;">
         <button id="tminus" type="button" class="circle minus" style="font-size: 22px;">-</button>
         <input value="1" name="raodenoba" class="quantity" style="color:black;margin:0px 30px;font-size: 1.1rem;">
         <button id="tplus" type="button" class="circle circle2 plus" style="font-size: 15px;">+</button>
       </p>
     </div>
-=======
-  </div>
-  <div class = "col-2 ticket_padding_top">
-    <p style = "font-size:1rem;padding-top:6px;"><span style="color:black;">
-    <input id = "tbilisi1_price" style = "background-color:#F8F8F8;border:none;display:inline-block;width:50px;" name="price" type="text" value="50" ></span>&nbsp;ლარი</p>
-  </div>
-  
-  <div class = "col-2 ticket_padding_top">
-    <button  type ="submit" class = "btn btn-success" data-toggle="modal" data-target="#exampleModalCenter1">ყიდვა</button>
-  </div>
-  </div>
-  
-</form>
->>>>>>> 8929884cff3a696331aa5a74f8768f6fb42a426a
 
     <div class = "col-12 col-sm-6 col-md-4 col-xl-2 d-flex justify-content-center ticket_padding_top align-items-center">
       <p style = "font-size:1rem;padding-top:6px;"><span style="color:black;">
@@ -187,7 +172,7 @@
     <p style = "font-size:1rem;padding-top:6px;"><span style="color:black;"><input id = "kaxeti_price" style = "background-color:#F8F8F8;border:none;display:inline-block;width:50px;" name="cash_f" type="text" value="" placeholder="30"></span>&nbsp;ლარი</p>
   </div>
   <input type="hidden" name="sec" value="<?=md5(md5(time())."flytaxi");?>">
-  <div class = "col-12 col-md-4 col-xl-2 ticket_padding_top d-flex justify-content-center align-items-start ticket_padding_top">
+  <div class = "col-12 col-md-4 col-xl-2 d-flex justify-content-center align-items-start ticket_padding_top">
     <button name = "submit" type ="button" class = "btn btn-success" id="blackseaButton" data-toggle="modal" data-target="#exampleModalCenter1">ყიდვა</button>
   </div>
   </div>
@@ -232,7 +217,7 @@
     <p style = "font-size:1rem;padding-top:6px;"><span style="color:black;"><input id = "kaxeti_price" style = "background-color:#F8F8F8;border:none;display:inline-block;width:50px;" name="cash_f" type="text" value="" placeholder="50"></span>&nbsp;ლარი</p>
   </div>
   <input type="hidden" name="sec" value="<?=md5(md5(time())."flytaxi");?>">
-  <div class = "col-12 col-md-4 col-xl-2 ticket_padding_top d-flex justify-content-center align-items-start ticket_padding_top">
+  <div class = "col-12 col-md-4 col-xl-2 d-flex justify-content-center align-items-start ticket_padding_top">
     <button name = "submit" type ="button" class = "btn btn-success" id="blackseaButton1" data-toggle="modal" data-target="#exampleModalCenter1">ყიდვა</button>
   </div>  </div>
 
@@ -280,7 +265,7 @@
     <p style = "font-size:1rem;padding-top:6px;"><span style="color:black;"><input id = "kaxeti_price" style = "background-color:#F8F8F8;border:none;display:inline-block;width:50px;" name="cash_f" type="text" value="" placeholder="25"></span>&nbsp;ლარი</p>
   </div>
   <input type="hidden" name="sec" value="<?=md5(md5(time())."flytaxi");?>">
-  <div class = "col-12 col-md-4 col-xl-2 ticket_padding_top d-flex justify-content-center align-items-start ticket_padding_top">
+  <div class = "col-12 col-md-4 col-xl-2 d-flex justify-content-center align-items-start ticket_padding_top">
     <button name = "submit" type ="button" class = "btn btn-success" data-toggle="modal" data-target="#exampleModalCenter1">ყიდვა</button>
   </div>
   </div>
@@ -302,7 +287,7 @@
     <p style = "font-size:1rem;padding-top:6px;"><span style="color:black;"><input id = "kaxeti_price" style = "background-color:#F8F8F8;border:none;display:inline-block;width:50px;" name="cash_f" type="text" value="" placeholder="40"></span>&nbsp;ლარი</p>
   </div>
   <input type="hidden" name="sec" value="<?=md5(md5(time())."flytaxi");?>">
-  <div class = "col-12 col-md-4 col-xl-2 ticket_padding_top d-flex justify-content-center align-items-start ticket_padding_top">
+  <div class = "col-12 col-md-4 col-xl-2 d-flex justify-content-center align-items-start ticket_padding_top">
     <button name = "submit" type ="button" class = "btn btn-success" data-toggle="modal" data-target="#exampleModalCenter1">ყიდვა</button>
   </div>  </div>
 </form>
