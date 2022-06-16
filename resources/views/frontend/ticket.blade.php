0476ec75bc5902d4e886e9bdbd42f266b9387654
securit8 <otar.porchxidze@gmail.com>
2022-06-16 18:13:26 +0400

    with lang adn mail fix ფიხ

--- resources/views/frontend/ticket.blade.php
+++ resources/views/frontend/ticket.blade.php
@@ -49,7 +49,7 @@
 <div>{{!!$raodenoba!!}} </div>
  <div class = "col-12 mt-3">
  <p>QR code:</p>
- <div class="mb-3">{!! DNS2D::getBarcodeHTML($qr, 'QRCODE') !!}</div>
+ <div class="mb-3"><img src="data:image/png,' . DNS1D::getBarcodePNG('234234324', 'C39+') . '" alt="barcode"   /></div>
  </div>
 </div>
 </div>
